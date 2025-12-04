ab4d551c7532ff67e94cd75f40ce0a7b16d8cb81
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2025-12-04 23:15:17 +0100

    Test maj modal 2

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -28,9 +28,9 @@
 
 import { useEffect } from "react";
 import "./Modal.css";
+console.log("Modal depuis NPM 🚀");
 
 export default function Modal({ isOpen, onClose, title, children }) {
-  console.log("Modal depuis NPM ");
   // Écoute de la touche Échap pour fermer la modale, comme avec jquery-modal
   useEffect(() => {
     if (!isOpen) return;
